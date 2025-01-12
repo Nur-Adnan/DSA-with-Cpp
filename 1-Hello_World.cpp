0c6de1d398a17b2e6334cd6957ba1806bf60ec53
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-12 18:34:50 +0600

    assignment complete

--- 1-Hello_World.cpp
+++ 1-Hello_World.cpp
@@ -4,3 +4,18 @@ int main(){
     cout << "Hello World" << endl;
     return 0;
 }
+
+/*
+    1. In C++, return -1 is often used in functions to indicate an error, 
+    failure, or some exceptional condition. 
+
+    2. The #include preprocessor directive in C++ is used to include 
+    the contents of a file (usually a header file) into the source code
+    at the point where the directive appears. It is typically used to 
+    include standard library headers or user-defined headers.
+
+    3.In C++, std is the namespace that contains the standard library 
+    features provided by the language. It stands for standard and encapsulates 
+    all standard library functions, classes, and objects to avoid name 
+    conflicts with user-defined functions or classes.
+*/
