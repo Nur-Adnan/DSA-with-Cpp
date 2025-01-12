43e4dbe0e45f1caf5acd79ee98b7c9d05b2996da
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-12 18:51:19 +0600

    variable declaration and definition completed

---
+++ 2-Variables_and_Datatypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // Variable Define
+    // int age = 25;
+    // cout << "My age is: " << age << endl;
+
+    // Variable declaration
+    // int age;
+    // cout << age;
+
+    // Definition
+    int age = 21;
+    cout << age;
+
+    return 0;
+}
