a5f483ede97d5a30af9bc5975f91471f495b2055
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-01-14 23:02:26 +0600

    updated

--- 2-Variables_and_Datatypes.cpp
+++ 2-Variables_and_Datatypes.cpp
@@ -11,7 +11,16 @@ int main(){
 
     // Definition
     int age = 21;
-    cout << age;
+    cout << age << endl;;
+
+    // manipulation or updating
+    age = 20;
+    cout << age << endl;
+
+    // int
+    int count = 1;
+    // float
+    int float = 1.21;
 
     return 0;
 }
