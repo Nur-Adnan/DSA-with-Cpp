82258f5f751d381044244bc61f3408a14731c6ff
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-17 14:13:00 +0600

    optimization completed

--- hollow_half_pyramid.cpp
+++ hollow_half_pyramid.cpp
@@ -4,17 +4,29 @@ int main(){
     int n;
     cout << "Enter the length" << endl;
     cin >> n;
+    // for(int row = 0; row < n; row++){
+    //     int totalCol = row + 1;
+    //     for(int col = 0; col < totalCol; col++){
+    //         if(row == 0 || row == 1 || row == n - 1){
+    //             cout << "* ";
+    //         }else{
+    //             if(col == 0 || col == totalCol -1){
+    //                 cout << "* ";
+    //             }else{
+    //                 cout << "  ";
+    //             }
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+    // optimization
     for(int row = 0; row < n; row++){
         int totalCol = row + 1;
         for(int col = 0; col < totalCol; col++){
-            if(row == 0 || row == 1 || row == n - 1){
+            if(row == 0 || row == 1 || row == n - 1 || col == 0 || row == col){
                 cout << "* ";
             }else{
-                if(col == 0 || col == totalCol -1){
-                    cout << "* ";
-                }else{
-                    cout << "  ";
-                }
+                cout << "  ";
             }
         }
         cout << endl;
