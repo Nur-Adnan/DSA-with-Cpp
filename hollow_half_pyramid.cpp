20f51c2b32cbacc74f8e0dd88f4e48da180c7d36
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-17 14:06:04 +0600

    half_inverted_pyramid_completed

---
+++ hollow_half_pyramid.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the length" << endl;
+    cin >> n;
+    for(int row = 0; row < n; row++){
+        int totalCol = row + 1;
+        for(int col = 0; col < totalCol; col++){
+            if(row == 0 || row == 1 || row == n - 1){
+                cout << "* ";
+            }else{
+                if(col == 0 || col == totalCol -1){
+                    cout << "* ";
+                }else{
+                    cout << "  ";
+                }
+            }
+        }
+        cout << endl;
+    }
+}
