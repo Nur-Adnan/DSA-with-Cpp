0933ee235860dfc32a77718b3c882586de1753a8
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-17 12:29:17 +0600

    updated

--- half_pyramid.cpp
+++ half_pyramid.cpp
@@ -6,7 +6,7 @@ int main(){
     cout << "Enter the length" << endl;
     cin >> n;
     for(int row = 0; row < n; row++){
-        for(int col = 0; col <= row; col++){
+        for(int col = 0; col < row + 1; col++){
             cout << "* ";
         }
         cout << endl;
