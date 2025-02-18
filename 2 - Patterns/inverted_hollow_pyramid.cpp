b8fa617e9849cbe857f60d9c9d3f274109f8620d
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-19 00:57:56 +0600

    completed inverted hollow pyramid

--- 2 - Patterns/inverted_hollow_pyramid.cpp
+++ 2 - Patterns/inverted_hollow_pyramid.cpp
@@ -4,5 +4,20 @@ int main(){
     int n; 
     cout << "Enter the Length: ";
     cin >> n;
-    
+    for(int row = 0; row < n; row++){
+        //space
+        for(int col = 0; col < row; col++){
+            cout << " ";
+        }
+        // starts
+        for(int col = 0; col < n - row; col++){
+            if(row == 0 || row == n - 1 || col == 0 || col == n - row - 1){
+                cout << "* ";
+            }else{
+                cout << "  ";
+            }
+            
+        }
+        cout << endl;
+    }
 }
