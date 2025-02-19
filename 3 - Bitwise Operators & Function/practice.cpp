d70d6d3994f4c953bb05c0511a4c2264235910d4
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-19 21:26:15 +0600

    left shift updated

--- 3 - Bitwise Operators & Function/practice.cpp
+++ 3 - Bitwise Operators & Function/practice.cpp
@@ -22,9 +22,13 @@ int main(){
     // 0 0 0 0 0 1 1 0 -> 1 + 1 = 2 that's binary form 0010
     // -6
     cout << ~(5) << endl;
-    cout << (5^5) << endl;
-
-
+    cout << (5^5^9) << endl;
+    // Left shit
+    // 25 * 2 to the power n
+    // that means here 2 to the power 1
+    cout << (25 << 1) << endl;
+    // that means here 2 to the power 2
+    cout << (25 << 2) << endl;
 
     return 0;
 }
