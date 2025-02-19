9f0e358d3b841a94e984ecffaec1669414bb660b
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-19 13:42:09 +0600

    full fancy pattern completed

---
+++ 2 - Patterns/full_facny_pattern.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the Length: ";
+    cin >> n;
+    for(int row = 0; row < n; row++){
+        for(int col = 0; col < 2 * row + 1; col++){
+            if(col % 2 == 0){
+                cout << row + 1 << " ";
+            }else{
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+    n--;
+    for(int row = 0; row < n; row++){
+        for(int col = 0; col < 2 * (n - row) - 1; col++){
+            if(col % 2 == 0){
+                cout << n - row << " ";
+            }else{
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+}
