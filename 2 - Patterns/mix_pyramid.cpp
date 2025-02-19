53f8789423b8a6bc9837220f9452e1c32fd3a51e
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-19 12:19:28 +0600

    mix_pyramid_completed

--- 2 - Patterns/mix_pyramid.cpp
+++ 2 - Patterns/mix_pyramid.cpp
@@ -4,5 +4,36 @@ int main(){
     int n;
     cout << "Enter the length: ";
     cin >> n;
-     
+    // Part - 1
+    for(int row = 0; row < n; row++){
+        //starts
+        for(int col = 0; col < n - row; col++){
+            cout << "* ";
+        }
+        //spaces
+        for(int col = 0; col < 2 * row + 1; col++){
+            cout << "  ";
+        }
+        //starts
+        for(int col = 0; col < n - row; col++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    //part - 2
+    for(int row = 0; row < n; row++){
+        //starts
+        for(int col = 0; col < row + 1; col++){
+            cout << "* ";
+        }
+        //spaces
+        for(int col = 0; col < 2 * (n - row) - 1; col++){
+            cout << "  ";
+        }
+        //starts
+        for(int col = 0; col < row + 1; col++){
+            cout << "* ";
+        }
+        cout << endl;
+    } 
 }
