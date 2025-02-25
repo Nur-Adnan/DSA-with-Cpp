74343fa7bf2732a934d769818cb3fe412693e382
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-25 17:31:49 +0600

    updated

--- 3 - Bitwise Operators & Function/pattern.cpp
+++ 3 - Bitwise Operators & Function/pattern.cpp
@@ -24,7 +24,7 @@ int main(){
     for(int row = 0; row < n; row++){
         for(int col = 0; col < row + 1; col++){
             cout << number;
-            number ++;
+            number ++ ; 
         }
         cout << endl;
     }
