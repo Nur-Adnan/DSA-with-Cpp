650a03de9b7d6f60c640df96334e12be86bc5fad
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-02-25 13:19:23 +0600

    pattern completed

---
+++ 3 - Bitwise Operators & Function/pattern.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+/*
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+
+*/
+
+
+/*
+    pass by value and pass by referance
+    pass by value = copy
+    pass by referance = actual value changes
+*/
+
+int main(){
+    int n;
+    cout << "Enter the length: ";
+    cin >> n;
+    int number = 1;
+    for(int row = 0; row < n; row++){
+        for(int col = 0; col < row + 1; col++){
+            cout << number;
+            number ++;
+        }
+        cout << endl;
+    }
+}
